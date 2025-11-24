e8cd8d9285ac6c6c149ca75175820379fe5585d2
Sapnendra Jaiswal <sapnendrajaiswal2388@gmail.com>
2025-11-24 16:34:15 +0530

    Add recent changes

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// home route
+app.get('/', (req, res) => {
+  res.send('Welcome to ByteBloom Agency Backend API');
+});
+
 // Routes
 app.use('/api/leads', leadRoutes);
 app.use('/api/admin', adminRoutes);
